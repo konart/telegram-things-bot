d75cb50a01b7aec953b26f5c3fd911db9e84ac8a
Artem Kondratev <konapt@gmail.com>
2018-02-18 21:17:17 +0300

    Added heroku listener

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+	"net"
+)
 
 func main() {
 
 	bot := setUpBot()
 	runBot(bot)
+
+	log.Fatal(http.ListenAndServe(net.JoinHostPort("0.0.0.0", os.Getenv("PORT")), nil))
 
 }
